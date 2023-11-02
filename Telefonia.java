849ee9fd7724760ac8b7ec7ebd4d34fc99345085
Gabriel Sobral <Gabriel.Sobral1367@gmail.com>
2023-11-01 23:48:38 -0300

    initial commit: creating 'Telefonia' structure class

---
+++ Telefonia.java
@@ -0,0 +1,28 @@
+public class Telefonia {
+    private int numPrePagos;
+    private int numPosPagos;
+
+    public void cadastrarAssinante() {
+
+    }
+
+    public void fazerChamada() {
+
+    }
+
+    public void fazerRecarga() {
+
+    }
+
+    private void localizarPrePago(long cpf) {
+
+    }
+
+    private void localizarPosPago(long cpf) {
+
+    }
+
+    public void imprimirFaturas() {
+
+    }
+}
