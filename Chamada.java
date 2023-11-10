850d9d3b59472f87e166fc495cc12ec24cea7ff3
julia zinser <juzinser@bitbucket.org>
2023-11-10 19:59:51 -0300

    adicionei os atributos e o constructor a classe Chamada

--- Chamada.java
+++ Chamada.java
@@ -1,3 +1,16 @@
+import java.util.GregorianCalendar;
+import java.text.SimpleDateFormat;
+
 public class Chamada{
+    //atributos
+    private GregorianCalendar data;
+    private int duracao;
+
+    //constructor
+    public Chamada(GregorianCalendar data, int duracao){
+        this.data = data;
+        this.duracao = duracao;
+    }
+
     
 }
