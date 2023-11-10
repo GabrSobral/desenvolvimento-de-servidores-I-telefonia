e442c74f5110f8752863512283d784b91d5413a4
julia zinser <juzinser@bitbucket.org>
2023-11-10 20:00:43 -0300

    adicionei os metodos a classe chamada

--- Chamada.java
+++ Chamada.java
@@ -11,6 +11,23 @@ public class Chamada{
         this.data = data;
         this.duracao = duracao;
     }
+    //métodos
 
+    //método que retorna a data
+    public GregorianCalendar getData(){
+        return data;
+    }
+
+    //retorna a duração
+    public int getDuracao(){
+        return duracao;
+    }
+    
+    //cria uma representação visual do atributos
+    public String toString(){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        String formattedDate = dateFormat.format(data.getTime());
+        return "Data: " + formattedDate + "\nDuração: " + duracao + " minutos";
+    }
     
 }
