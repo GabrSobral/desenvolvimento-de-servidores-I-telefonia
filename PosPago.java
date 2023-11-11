2364aff805780cd003235b3cbee8e5e9ee54a62a
Carlos Santana <carlos.santana33890@gmail.com>
2023-11-11 11:57:20 -0300

    feat: class PrePago and PosPago

---
+++ PosPago.java
@@ -0,0 +1,16 @@
+import java.util.GregorianCalendar;
+
+public class PosPago extends Assinante {
+  private float assinatura;
+
+  public PosPago(String cpf, String nomeAssinante, int celularAssinante, float assinatura) {
+    super(cpf, nomeAssinante, celularAssinante);
+    this.assinatura = assinatura;
+  }
+
+  public void fazerChamada(GregorianCalendar data, int duracao) {
+  }
+
+  public void imprimirFatura(int mes) {
+  }
+}
---
+++ PrePago.java
@@ -0,0 +1,23 @@
+import java.util.GregorianCalendar;
+
+public class PrePago extends Assinante {
+    private Recarga recargas[];
+    private float creditos;
+    private int numRecargas;
+
+    public PrePago(String cpf, String nome, int celularAssinante) {
+        super(cpf, nome, celularAssinante);
+    }
+
+    public void recarregar(GregorianCalendar data, float valor) {
+
+    }
+
+    public float fazerChamada(GregorianCalendar data, int duracao) {
+        return 0F;
+    };
+
+    public void imprimirFatura(int mes) {
+
+    };
+}
