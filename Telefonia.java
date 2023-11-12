fd14b5479a074a59e78ffb98a039b710047be04c
Gabriel Sobral <Gabriel.Sobral1367@gmail.com>
2023-11-12 16:42:20 -0300

    feat: creating 'cadastrarAssinante' method at 'Telefonia' class

--- Telefonia.java
+++ Telefonia.java
@@ -1,8 +1,42 @@
+import java.util.Scanner;
+
 public class Telefonia {
     private int numPrePagos;
     private int numPosPagos;
 
+    private PrePago[] prePagos; // change type later
+    private PosPago[] posPagos; // change type later
+
+    public Telefonia() {
+        this.numPosPagos = 0;
+        this.numPrePagos = 0;
+
+        prePagos = new PrePago[10];
+        posPagos = new PosPago[10];
+    }
+
     public void cadastrarAssinante() {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] registerSubscriberOptions = { "1 - Pós-pago", "2 - Pré-pago", };
+        View.printMenu(registerSubscriberOptions);
+
+        int selectedOption = scanner.nextInt();
+
+        if(selectedOption == 1) {
+            PosPago newPostPaidSubscriber = View.getPostPaidSubscriberDataByView(scanner);
+
+            posPagos[numPosPagos] = newPostPaidSubscriber;
+            numPosPagos++;
+        } else {
+            PrePago newPrePaidSubscriber = View.getPrePaidSubscriberDataByView(scanner);
+
+            prePagos[numPrePagos] = newPrePaidSubscriber;
+            numPrePagos++;
+        }
+    }
+
+     public void listarAssinantes() {
 
     }
 
@@ -16,13 +50,13 @@ public class Telefonia {
 
     private void localizarPrePago(long cpf) {
 
-    }
+    }  
 
     private void localizarPosPago(long cpf) {
 
     }
 
     public void imprimirFaturas() {
-
+        
     }
 }
