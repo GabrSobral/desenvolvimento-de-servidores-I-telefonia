e5e56e99c915c45088e0b6b8d2e1cc007daa9003
Lucas Gradilone <lucasgradilone@gmail.com>
2023-11-13 10:26:08 -0300

    feat: Create method constructor and variables

--- Recarga.java
+++ Recarga.java
@@ -1,3 +1,11 @@
-public class Recarga {
+import java.util.GregorianCalendar;
 
+public class Recarga {
+    private GregorianCalendar data;
+    private float valor;
+
+    public Recarga(GregorianCalendar data, float valor){
+        this.data = data;
+        this.valor = valor;
+    }
 }
